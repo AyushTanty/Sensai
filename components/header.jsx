076f4e4fbf8ffcd8365ca3febd9ab25c1622c19b
ASUS <ayushtanty003@gmail.com>
2025-10-27 23:12:05 +0530

    header changes

--- components/header.jsx
+++ components/header.jsx
@@ -1,21 +1,73 @@
 import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import { Button } from './ui/button'
+import { ChevronDown, FileText, GraduationCap, LayoutDashboard, PenBox, StarIcon } from 'lucide-react'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from './ui/dropdown-menu'
 
 const Header = () => {
   return (
-    <div>
-      <SignedOut>
-            <SignInButton/>
-                <SignUpButton>
-                    <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                    Sign Up
-                    </button>
-               </SignUpButton>
+    <header className='fixed top-0 w-full border-b bg-background/80  backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60'>
+      <nav className='container mx-auto px-0 h-16 flex items-center justify-between'> 
+       <Link href={"/"}>
+           <Image src="/logo.png" alt='sensai logo' 
+             width={200} 
+             height={60}
+             className='h-25 py-1 w-auto object-contain'
+           />
+       </Link>
+
+       <div className='flex items-center space-x-2 md:space-x-4'>
+        {/* show when person is SignedIn */}
+          <SignedIn>
+             <Link href={"/dashboard"}>
+               <Button variant={"outline"}>
+                 <LayoutDashboard className='h-4 w-4'/>
+                 <span className='hidden md:block'>Industry Insights</span>
+                </Button>
+             </Link>
+          
+
+          <DropdownMenu>
+            <DropdownMenuTrigger><Button>
+                 <StarIcon className='h-4 w-4'/>
+                 <span className='hidden md:block'>Growth Tools</span>
+                 <ChevronDown className='h-4 w-4' />
+                 </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem>
+                <Link href={"./resume"} className='flex items-center gap-2'> 
+                  <FileText className='h-4 w-4'/>
+                  <span>Growth Tools</span>
+                 </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem><Link href={"./ai-cover-letter"} className='flex items-center gap-2'> 
+                  <PenBox className='h-4 w-4'/>
+                  <span>Cover Letter</span>
+                 </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem><Link href={"./interview"} className='flex items-center gap-2'> 
+                  <GraduationCap className='h-4 w-4'/>
+                  <span>Interview Prep</span>
+                 </Link>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          </SignedIn>
+
+          <SignedOut>
+            <SignInButton>
+              <Button variant={"outline"}>Sign In</Button>
+            </SignInButton>
         </SignedOut>
         <SignedIn>
             <UserButton />
         </SignedIn>
-    </div>
+       </div>
+      </nav>
+    </header>
   )
 }
 
